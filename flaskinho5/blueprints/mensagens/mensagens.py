45eb29843c6cbde581bbdc4c310a9c36ebe625ce
Felipesaraiva113 <fe.emanuelsaraiva@gmail.com>
2026-04-24 23:01:33 -0300

    Instalação de dependências e preparação para o novo SO

--- flaskinho5/blueprints/mensagens/mensagens.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-mensagens_bp = Blueprint('mensagens', __name__, template_folder='templates',static_folder='static',static_url_path='/mensagens/static')
-@mensagens_bp.route('/ola')
-def ola():
-    return render_template('ola.html')
-@mensagens_bp.route('/oi')
-def oi():
-    return 'oi!'
